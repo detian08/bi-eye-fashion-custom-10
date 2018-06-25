03ebb84bbf92e617af2e951d473cfbb58d0b9a46
Your Name <you@example.com>
2018-06-25 21:15:52 +0200

    [ADD] new module for customizations

---
+++ eye_fashion/eyefashion_custom/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Eye-fashion Custom',
+    'category': 'Sales',
+    'author': "Emantaha@bisolutions.tech",
+    'website': 'http://www.acme-group.net',
+    'depends': ['eyefashion_sale'
+                ],
+    'data': [
+            'views/res_partner.xml',
+             ],
+    'installable': True,
+}
